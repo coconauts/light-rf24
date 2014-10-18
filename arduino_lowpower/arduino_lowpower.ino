b719432f98fa08962665e28162f98cba4e67c2e6
rephus <rephus@gmail.com>
2014-10-18 11:26:30 +0100

    Remove trans and led pins

--- arduino_lowpower/arduino_lowpower.ino
+++ arduino_lowpower/arduino_lowpower.ino
@@ -6,36 +6,36 @@
 #define LDR 0 // Select input pin for the photocell light sensor.
 #define FREQ 60000 //Frequency of readings (in ms)
 
-#define LED 4 //indicator led
-#define TRANS 8 //transinstor pin (base) to reduce consumption while sleeping
+#define POWER 8 //power pin to reduce consumption while sleeping
 
 int val = 0; // Variable to store the value coming from the sensor.
 
 //rf24
+#define DEVICE_ID 1
 #define CHANNEL 1 //MAX 127
 RF24 radio(9,10);
-byte pipes[][6] = {"1Node","2Node"};
+byte pipe[]= {"light"};
  
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
+
+#define MAX_SENSOR_VALUE 1023
+#define MAX_RADIO_VALUE 254
 
 void setup() {
     
   pinMode(LDR, INPUT); // Declare LDR as an input pin.
-  pinMode(LED, OUTPUT); 
-  pinMode(TRANS, OUTPUT);  
+  pinMode(POWER, OUTPUT);  
 }
  
 void loop() {
-  digitalWrite(TRANS, HIGH);
+  digitalWrite(POWER, HIGH);
   val = analogRead(LDR); // Read current value from the sensor.
-  digitalWrite(LED, HIGH);
   
   restartRadio();
-  radio.write(&val , sizeof(int) ); //Send via radio
+  radio.write(&val , sizeof(val) ); //Send via radio
   stopRadio();
   
-  digitalWrite(LED, LOW);
-  digitalWrite(TRANS, LOW);
+  digitalWrite(POWER, LOW);
   Sleepy::loseSomeTime(FREQ);
 }
 
@@ -55,8 +55,6 @@ void restartRadio(){
   radio.setChannel(CHANNEL);
   radio.setAutoAck(1);     // Ensure autoACK is enabled
   radio.setRetries(15,15); // Max delay between retries & number of retries
-  radio.openWritingPipe(pipes[1]);
-  //radio.printDetails();  // Dump the configuration of the rf unit for debugging
+  radio.openWritingPipe(pipe);
   radio.stopListening();
-  //radio.setPALevel(RF24_PA_MAX);
 }
